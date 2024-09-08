4069f5c919ee1bbdbb7272b47176c123de776aae
Hubert Chen <hubertchen200@gmail.com>
2024-09-08 17:24:52 -0400

    Initial Commit

---
+++ ios-chat/ios_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_chatApp.swift
+//  ios-chat
+//
+//  Created by Hubert Chen on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
